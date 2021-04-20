256622320d467462324474376beecc2a07983935
Serj-L <svlepniakov@gmail.com>
2021-04-20 16:25:39 +0300

    add sections: Best workers, Reasons

--- #src/js/smooth-scroll-to-block.js
+++ #src/js/smooth-scroll-to-block.js
@@ -1,21 +1,30 @@
 // menu scroll to function
-const menuLinks = document.querySelectorAll('.header__nav-link[data-goto], .header__hero-btn[data-goto]');
+const menuLinks = document.querySelectorAll(
+  ".header__nav-link[data-goto], .header__hero-btn[data-goto]"
+);
+
 if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', function(e) {
-            const menuLink = e.target;
-            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-                const gotoBlock = document.querySelector(menuLink.dataset.goto);
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__logo-nav').offsetHeight;
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", function (e) {
+      const menuLink = e.target;
+      if (
+        menuLink.dataset.goto &&
+        document.querySelector(menuLink.dataset.goto)
+      ) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue =
+          gotoBlock.getBoundingClientRect().top +
+          pageYOffset -
+          document.querySelector(".header__logo-nav").offsetHeight;
 
-                window.scrollTo({
-                    top: gotoBlockValue,
-                    behavior: 'smooth'
-                });
-
-                e.preventDefault();
-            }
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: "smooth",
         });
+
+        e.preventDefault();
+      }
     });
+  });
 }
 // menu scroll to function
--- WebTech/js/script.js
+++ WebTech/js/script.js
@@ -45,26 +45,36 @@ scrBtn.addEventListener("click", function (e) {
 // scroll up
 ;
 // menu scroll to function
-const menuLinks = document.querySelectorAll('.header__nav-link[data-goto], .header__hero-btn[data-goto]');
+const menuLinks = document.querySelectorAll(
+  ".header__nav-link[data-goto], .header__hero-btn[data-goto]"
+);
+
 if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', function(e) {
-            const menuLink = e.target;
-            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-                const gotoBlock = document.querySelector(menuLink.dataset.goto);
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__logo-nav').offsetHeight;
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", function (e) {
+      const menuLink = e.target;
+      if (
+        menuLink.dataset.goto &&
+        document.querySelector(menuLink.dataset.goto)
+      ) {
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue =
+          gotoBlock.getBoundingClientRect().top +
+          pageYOffset -
+          document.querySelector(".header__logo-nav").offsetHeight;
 
-                window.scrollTo({
-                    top: gotoBlockValue,
-                    behavior: 'smooth'
-                });
-
-                e.preventDefault();
-            }
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: "smooth",
         });
+
+        e.preventDefault();
+      }
     });
+  });
 }
-// menu scroll to function;
+// menu scroll to function
+;
 const burgerMenu = document.querySelector('.header__burger-btn');
 const menuWrapper = document.querySelector('.header__nav');
 const wrapper = document.querySelector('.header__logo-nav-wrapper');
