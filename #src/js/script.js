938556064f808e29f2f2b314fb0addc59ddef1a0
Serj-L <svlepniakov@gmail.com>
2021-05-14 10:27:17 +0300

    add sections: Gallery, Testimonials

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,6 +4,7 @@
 @@include("scroll-up.js");
 @@include("smooth-scroll-to-block.js");
 @@include("burger.js");
+@@include('swiper-bundle.min.js')
 
 //check broswer webP support
 function testWebP(callback) {
@@ -27,3 +28,50 @@ window.onscroll = function () {
   scrollUp();
   stickyNav();
 };
+
+//testimonials slider
+const swiperTestimonialsCustomers = new Swiper('.testimonials__customers', {
+  // parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  centeredSlides: true,
+  speed: 700,
+
+  breakpoints: {
+    320: {slidesPerView: 1},
+    576: {slidesPerView: 3},
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-next"
+  },
+//spaceBetween: 20,
+});
+
+const testimonialsSlides = document.querySelectorAll('.testimonials__customers-slide');
+const testimonialsSlidesImg = document.querySelectorAll('.testimonials__customers-img');
+const testimonialsReview = document.querySelectorAll('.testimonials__review-slide-wrapper');
+
+swiperTestimonialsCustomers.on('slideChange', function () {
+  testimonialsSlidesImg.forEach(image => {
+    if (Number(image.closest('.testimonials__customers-slide').dataset.swiperSlideIndex) === swiperTestimonialsCustomers.realIndex) {
+      image.classList.add('testimonials__customers-img--active');
+    } else {
+      image.classList.remove('testimonials__customers-img--active');
+    }
+  });
+
+  testimonialsSlides.forEach(slide => {
+    if (Number(slide.dataset.swiperSlideIndex) === swiperTestimonialsCustomers.realIndex) {
+      testimonialsReview.forEach(review => {
+        if (Number(review.dataset.reviewId) === Number(slide.dataset.customerId)) {
+          review.classList.add('testimonials__review-slide-wrapper--active');
+        } else {
+          review.classList.remove('testimonials__review-slide-wrapper--active');
+        }
+      })
+    }
+  });
+});
